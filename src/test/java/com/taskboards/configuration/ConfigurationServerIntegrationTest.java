bda53bf470629e6960e05c227b22aa34ddbf6e68
leoeberts <leonardo.eberts@gmail.com>
2017-12-03 23:33:55 -0200

    Test configuration

--- src/test/java/com/taskboards/configuration/ConfigurationServerIntegrationTest.java
+++ src/test/java/com/taskboards/configuration/ConfigurationServerIntegrationTest.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -19,6 +20,7 @@ import org.springframework.test.web.servlet.MockMvc;
 @AutoConfigureMockMvc
 @SpringBootTest(classes = ConfigurationApplication.class,
                 webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@ActiveProfiles("test")
 public class ConfigurationServerIntegrationTest {
 	
 	@Autowired
