d906f9b16e569a9a564676b3de987d053a26515d
leoeberts <leonardo.eberts@gmail.com>
2017-12-03 22:01:18 -0200

    Configuration discovery

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: java --server.port=$PORT
--- src/main/java/com/taskboards/configuration/ConfigurationApplication.java
+++ src/main/java/com/taskboards/configuration/ConfigurationApplication.java
@@ -3,9 +3,11 @@ package com.taskboards.configuration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 //TODO: bus/rabbitmq
 @EnableConfigServer
+@EnableEurekaClient
 @SpringBootApplication
 public class ConfigurationApplication {
 
